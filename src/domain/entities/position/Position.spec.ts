770b789afa9ad7cd97890162fd5f28b95375d754
Joao Azevedo <joaovarieira@gmail.com>
2026-06-10 13:02:31 -0300

    test: add Position factory tests

---
+++ src/domain/entities/position/Position.spec.ts
@@ -0,0 +1,21 @@
+import { describe, expect, test } from "vitest";
+import { Position } from "./Position";
+import { InvalidPositionError } from "../errors/InvalidPositionError";
+
+describe("Position", () => {
+  test("must return a new instance when two integer numbers are used on create", () => {
+    const col = 1;
+    const row = 2;
+    const position = Position.create(col, row);
+    expect(position.col).equals(col);
+    expect(position.row).equals(row);
+  });
+
+  test("must throw when col is a float number on create", () => {
+    expect(() => Position.create(1.5, 0)).toThrow(InvalidPositionError);
+  });
+
+  test("must throw when row is a float number on create", () => {
+    expect(() => Position.create(0, 1.5)).toThrow(InvalidPositionError);
+  });
+});
