6062210cac75ed302002848501e154660e00ee0b
Joao Azevedo <joaovarieira@gmail.com>
2026-07-22 11:07:14 -0300

    feat: adding vercel's analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
+import { Analytics } from "@vercel/analytics/react";
 import { router } from "./presentation/router";
 import { initTheme } from "./presentation/theme/theme";
 import "./presentation/i18n/i18n";
@@ -11,5 +12,6 @@ initTheme();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
+    <Analytics />
   </StrictMode>,
 );
